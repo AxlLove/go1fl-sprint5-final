604ab2c3605ec859adae2e0c58c106e2cb1fde7a
am_kitaev <am.kitaev@rusagro.tech>
2026-03-31 22:13:40 +0300

    feat: implement tracker iteration 1

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,32 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	if len(dataset) == 0 {
+		log.Println("dataset is empty")
+		return
+	}
+
+	for _, v := range dataset {
+		err := dp.Parse(v)
+		if err != nil {
+			log.Printf("error parsing item %s: %v", v, err)
+			continue
+		}
+	}
+
+	msg, err := dp.ActionInfo()
+	if err != nil {
+		log.Printf("error getting action info: %v", err)
+	}
+	fmt.Println(msg)
 }
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,61 @@
 package daysteps
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	personaldata.Personal
+
+	Steps    int
+	Duration time.Duration
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	const expParts = 2
+	parts := strings.Split(datastring, ",")
+
+	if len(parts) != expParts {
+		return fmt.Errorf("invalid input format: expected %d segments, got %d", expParts, len(parts))
+	}
+
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return fmt.Errorf("parse steps: %w", err)
+	}
+
+	if steps <= 0 {
+		return fmt.Errorf("steps must be positive, got %d", steps)
+	}
+
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return fmt.Errorf("parse duration: %w", err)
+	}
+	if duration <= 0 {
+		return fmt.Errorf("duration must be positive, got %d", duration)
+	}
+
+	ds.Steps = steps
+	ds.Duration = duration
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	spentCal, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", fmt.Errorf("calculate spent calories error: %w", err)
+	}
+
+	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", ds.Steps, distance, spentCal)
+
+	return result, nil
 }
